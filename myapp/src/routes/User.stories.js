cc4de53cdcc31baa6168d16cd27a337c9333be02
Russ Tuck <russ.tuck@gordon.edu>
2023-09-30 23:51:19 -0400

    Add test for User

---
+++ myapp/src/routes/User.stories.js
@@ -0,0 +1,28 @@
+// Counter.stories.js
+import User from './User.svelte';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+export default {
+    component: User,
+};
+
+export const SetName = {
+    render: (args) => ({
+        Component: User,
+    }),
+    play: async({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        // Name starts as Guest
+        await expect(canvas.getByDisplayValue('Guest')).toBeInTheDocument();
+
+        // Change name to "First Last"
+        await userEvent.type(canvas.getByDisplayValue('Guest'),
+                             '{backspace}{backspace}{backspace}{backspace}' +
+                             '{backspace}First Last');
+        await expect(canvas.getByDisplayValue('First Last'))
+            .toBeInTheDocument();
+        await expect(canvas.queryByDisplayValue('Guest')).toBeNull();
+    },
+};
